ecb0fb8cb70b8394b5ae0d51aab26bb7b099d5d2
Lithium <91525734+Lithium02@users.noreply.github.com>
2022-12-06 20:40:42 -0300

    Changed main script name to hornerito.py, added wikiscraper.

--- hornerito.py
+++ hornerito.py
@@ -1,14 +1,18 @@
-import datetime
+# requirements decouple, discord, bs4, requests, 
+# Dependencies imports
 from decouple import config
+import discord
+# Local scripts
+import wikiscraper
 
 token = config('TOKEN')
 
-import discord
-import random
+wiki_web = wikiscraper.WikiWeb()
+
+no_result_message = '''Sorry, we can\'t find what you are searching for.'''
 
 intents = discord.Intents.default()
 intents.message_content = True
-date = datetime.datetime.now()
 
 client = discord.Client(intents=intents)
 
@@ -20,9 +24,17 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    message_content = message.content.lower()  
+    
     # if message.content.startswith('$hello'):
     #     await message.channel.send('world!')
     if message.content.startswith('$definition'):
         await message.channel.send('definition')
+        
+    if f'$wiki' in message_content:
+        key_words, search_words = wiki_web.key_words_search_words(message_content)
+        result = wiki_web.search(key_words)
+        await message.channel.send(result.text)
 
 client.run(token)
